c93130bdb18804f7241042e13170fb0473a72e09
MI KE <fernandesmandrew@gmail.com>
2023-07-09 18:20:54 +0800

    Created typography style guide on a separate css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,25 +19,6 @@ export default {
       fontFamily: {
         base: "Plus Jakarta Sans, sans-serif",
       },
-
-      fontSize: {
-        "link-hovered": ["16px", { fontWeight: "bold" }],
-        price: ["25px", { letterSpacing: "-0.625px", fontWeight: "800" }],
-        tags: [
-          "13px",
-          {
-            letterSpacing: "0.13px",
-            fontWeight: "800",
-          },
-        ],
-
-        body: ["16px", { lineHeight: "145%", fontWeight: "500" }],
-        link: "16px",
-
-        title: "25px",
-        "headings-a": ["39px", { lineHeight: "110%" }],
-        "headings-b": ["31px", { lineHeight: "110%" }],
-      },
     },
   },
   plugins: [],
