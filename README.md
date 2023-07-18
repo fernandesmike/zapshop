03e3b07acfb2e722d15375b0797ad084dcaed5fa
MI KE <fernandesmandrew@gmail.com>
2023-07-18 16:35:28 +0800

    Added Autoplay plugin for carousel

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 3. Tailwind css
 4. React Router
 5. Outlets
+
+## Hooks
+
+1. Embla Carousel - for seamless and beautiful slideshows or carousel
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
 
 const Carousel = () => {
-  return <div>I am a big carousel</div>;
+  // Embla carousel has their integration hook
+  const [emblaRef] = useEmblaCarousel();
+
+  // Copied this snippet from documentation
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        <div className="embla__slide">Slide 1</div>
+        <div className="embla__slide">Slide 2</div>
+        <div className="embla__slide">Slide 3</div>
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
