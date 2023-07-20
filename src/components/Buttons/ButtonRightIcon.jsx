6d0f7545b0bf09fba311c8634c44c0fe1c35a6b1
MI KE <fernandesmandrew@gmail.com>
2023-07-20 14:44:55 +0800

    Added hover effect for ButtoRightIcon component

--- src/components/Buttons/ButtonRightIcon.jsx
+++ src/components/Buttons/ButtonRightIcon.jsx
@@ -5,7 +5,7 @@ import RightArrow from "../../assets/icons/arrow-right.svg";
 
 const ButtonRightIcon = ({ buttonTxt }) => {
   return (
-    <button className="flex justify-center gap-sm items-center min-w-[230px] max-w-[230px] py-4 bg-brand-accent text-white rounded">
+    <button className="flex justify-center gap-sm items-center min-w-[230px] max-w-[230px] py-4 bg-brand-accent text-white rounded hover:bg-brand-dark">
       {buttonTxt}
       <img src={RightArrow} alt="" />
     </button>
