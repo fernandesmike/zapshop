1753a997b5340646de9ca2d2e3851b25aea69c5a
MI KE <fernandesmandrew@gmail.com>
2023-07-20 17:34:55 +0800

    Created SingleRow and DualRow product listing components

---
+++ src/components/ProductListing/DualRow.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DualRow = () => {
+  return <div>DualRow</div>;
+};
+
+export default DualRow;
---
+++ src/components/ProductListing/SingleRow.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleRow = () => {
+  return <div>SingleRow</div>;
+};
+
+export default SingleRow;
