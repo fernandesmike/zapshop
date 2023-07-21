82a7623e16d75f7b896f109bcd19780dbce2c726
MI KE <fernandesmandrew@gmail.com>
2023-07-21 19:19:30 +0800

    Finished ExclusiveDeals page

--- src/pages/ExclusiveDeals/ExclusiveDeals.jsx
+++ src/pages/ExclusiveDeals/ExclusiveDeals.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+// Components
+import DualRow from "../../components/ProductListing/DualRow";
+
 const ExclusiveDeals = () => {
-  return <div>Test</div>;
+  return (
+    <div className="flex flex-col gap-xl py-10">
+      {/* Deal banner container */}
+      <div>
+        <div className="flex flex-col items-center gap-md">
+          <h1 className="heading-a font-bold">Deals for you</h1>
+          <p className="body text-secondary">
+            You might like these amazing deals we've only just for you
+          </p>
+        </div>
+      </div>
+
+      <DualRow showTitle={false} showButton={false} />
+    </div>
+  );
 };
 
 export default ExclusiveDeals;
