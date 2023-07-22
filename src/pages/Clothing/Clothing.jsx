61f7c04c8ddee5de176f1bc58af27fa3635867ec
MI KE <fernandesmandrew@gmail.com>
2023-07-22 12:40:19 +0800

    Configured Clothes category page

--- src/pages/Clothing/Clothing.jsx
+++ src/pages/Clothing/Clothing.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Clothing = () => {
-  return <div>Clothing HEHEHEHE</div>;
+  return (
+    <div>
+      {/* Banner container */}
+      <div></div>
+
+      {/* Filters and Product lising container */}
+      <div></div>
+
+      {/* Pagination container */}
+      <div></div>
+    </div>
+  );
 };
 
 export default Clothing;
